bf9205c36ba301208dd61ea06eafa967a87d781f
tope-olajide <31278728+tope-olajide@users.noreply.github.com>
2022-04-13 19:29:29 +0100

    Add Mobile Touch Event

---
+++ pages/About.jsx
@@ -0,0 +1,12 @@
+import styles from "../styles/Home.module.css";
+
+export default function About() {
+    
+  return (
+    <>
+    <div className={styles.container}>
+     
+     </div>
+    </>
+  );
+}
---
+++ pages/HowToPlay.jsx
@@ -0,0 +1,12 @@
+import styles from "../styles/Home.module.css";
+
+export default function HowToPlay() {
+    
+  return (
+    <>
+    <div className={styles.container}>
+     
+     </div>
+    </>
+  );
+}
