fa0d1dc5309f8f8914a7d56d7437fbeb647eb745
tope-olajide <tope-olajide@users.noreply.github.com>
2022-04-14 12:45:01 +0100

    Update textToSpeech.js

--- pages/api/textToSpeech.js
+++ pages/api/textToSpeech.js
@@ -18,24 +18,15 @@ export default async function handler(req, res) {
         
          form.parse(req, (err, fields, files) => {
              if (err) return reject(err)
-//console.log(fields, files)
-            // console.log(files.file.filepath)
-             const oldPath = files.file.filepath;
-            // const newPath = `./public/sound-files/${files.file.originalFilename}`;
-             const newPath = `./${files.file.originalFilename}`;
-             mv(oldPath, newPath, function(err) {
-               return resolve (newPath)
-             });
-             /* const newPath = `./public/sound-files/${files.file.originalFilename}`
-             fs.writeFileSync("blob2", newPath); */
+             const filepath = files.file.filepath;
+             console.log(filepath)
+            return resolve (filepath)
             
-             
-            /*  res.status(200).json({ fields, files,oldPath }) */
          })
      })
-   const newPath = await data;
+   const filepath = await data;
    const streamSource = {
-    stream: fs.readFileSync(newPath),
+    stream: fs.readFileSync(filepath),
     mimetype: 'audio/wav',
     language:'en'
   };
@@ -44,8 +35,6 @@ export default async function handler(req, res) {
  const response = await deepgram.transcription.preRecorded(streamSource, {
   punctuate: false,
 }).then((response) => {
-  console.log ('url :-' +newPath)
-  // Write the response to the console
   console.log(response.results.channels[0].alternatives[0]);
  return res.status(200).json(response )
   
@@ -53,32 +42,7 @@ export default async function handler(req, res) {
   console.log(err);
  return res.status(400).json(err )
 })
- 
-     /*   
-        const url  = req.body
 
-
-     const { results } = await deepgram.transcription.preRecorded({
-      buffer: Buffer.from(parsedUrl, 'base64'),
-      mimetype: 'audio/webm'
-    }) 
-  const  testAudioRecord = URL.createObjectURL(url);
-console.log(testAudioRecord)
-     const { results } = await deepgram.transcription.preRecorded({ url })
-    res.status(200).json(testAudioRecord); 
-    
-          deepgram.transcription.preRecorded({ 
-            buffer: Buffer.from(url, 'base64') , 
-            mimetype: 'audio/wav'
-         })
-  .then((transcript) => {
-    console.log(transcript);
-    res.status(200).json({ transcript });
-  })
-  .catch((err) => {
-    console.log(err);
-    res.status(400).json({ err });
-  });  */ 
     }   
   }
   
